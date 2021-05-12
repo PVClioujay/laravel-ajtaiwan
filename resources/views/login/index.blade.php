90e32e320ae7458ea4c60114db95ef6c2314ab95
PVClioujay <llioujay@gmail.com>
2021-05-13 02:23:57 +0800

    變更註冊成功導向方式

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -1,34 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ajtaiwan</title>
-    @if ($msg)
-        <script>
-            alert('Registe Success');
-        </script>
-    @endif
 </head>
+
 <body>
     @guest
-        <form action="/login" method="POST">
-            @csrf
-            <div class="form-group">
-                <label for="UserName">帳號</label>
-                <input type="text" class="form-control" name="account" id="account" placeholder="Name">
-            </div>
-            <div class="form-group">
-                <label for="UserName">密碼</label>
-                <input type="password" class="form-control" name="pwd" id="pwd" placeholder="password">
-            </div>
-            <button type="submit" class="btn btn-primary" id="submit" onclick="submit()">送出</button>
-            <a href="/register">Don't have account?</a>
-        </form>
+    <form action="/login" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="UserName">帳號</label>
+            <input type="text" class="form-control" name="email" id="email" placeholder="email">
+        </div>
+        <div class="form-group">
+            <label for="UserName">密碼</label>
+            <input type="password" class="form-control" name="password" id="password" placeholder="password">
+        </div>
+        <button type="submit" class="btn btn-primary" id="submit" onclick="submit()">送出</button>
+        <a href="/register">Don't have account?</a>
+        @error('error')
+        <div class="alert alert-danger">{{ $errors->first('error') }}</div>
+        @enderror
+    </form>
     @endguest
     @auth
-        <h1>Login!</h1>
+    <h1>Login!</h1>
     @endauth
 </body>
+
 </html>
--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -6,6 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
+    @if ( $msg ?? '')
+    <script>
+        alert('Registe Success');
+            location.href = 'login';
+    </script>
+    @endif
 </head>
 
 <body>
@@ -13,23 +19,25 @@
         @csrf
         <div class="form-group">
             <label for="username">User Name:</label>
-            <input type="text" class="@error('account') is-invalid @enderror" id="account" aria-describedby="account" name="account">
+            <input type="text" class="@error('account') is-invalid @enderror" id="account" aria-describedby="account"
+                name="account">
             @error('account')
-                <div class="alert alert-danger">{{ $message }}</div>
+            <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
         <div class="form-group">
             <label for="password">Password:</label>
-            <input type="password" class="@error('pasword') is-invalid @enderror" id="password" aria-describedby="pasword" name="password">
+            <input type="password" class="@error('pasword') is-invalid @enderror" id="password"
+                aria-describedby="pasword" name="password">
             @error('password')
-                <div class="alert alert-danger">{{ $message }}</div>
+            <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
         <div class="form-group">
             <label for="email">Email:</label>
             <input id="email" type="email" class="@error('email') is-invalid @enderror" name="email">
             @error('email')
-                <div class="alert alert-danger">{{ $message }}</div>
+            <div class="alert alert-danger">{{ $message }}</div>
             @enderror
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
